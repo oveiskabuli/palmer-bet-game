40350880c56ad949671617766366d7425eb0b162
Oveis Kabuli <o.kabuli@bettechnology.com>
2024-12-09 10:21:50 +0330

    add game files and load it in power up cmp

--- src/app/power-up/power-up.component.ts
+++ src/app/power-up/power-up.component.ts
@@ -1,17 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare global {
+  interface Window {
+    handleUnityMessage: (message: any) => void;
+  }
+}
+
+declare function createUnityInstance(a: any, b: any, c: any): any;
 
 @Component({
   selector: 'app-power-up',
   templateUrl: './power-up.component.html',
   styleUrls: ['./power-up.component.scss'],
 })
-export class PowerUpComponent implements OnInit {
-  constructor(private _activatedRoute: ActivatedRoute) {}
+export class PowerUpComponent implements AfterViewInit {
+  @ViewChild('gameContainer') gameContainer?: ElementRef;
+  @ViewChild('gameCanvas') gameCanvas?: ElementRef;
+  @ViewChild('gameLoadingBar') gameLoadingBar?: ElementRef;
+  @ViewChild('gameProgressBarFull') gameProgressBarFull?: ElementRef;
+  unityInstance: any;
+  betId?: string;
+  baseUrl?: string;
 
-  ngOnInit(): void {
-    const betId = this._activatedRoute.snapshot.queryParamMap.get('betId');
-    const baseUrl = this._activatedRoute.snapshot.queryParamMap.get('baseUrl');
-    console.log(betId, baseUrl);
+  constructor(
+    private _renderer: Renderer2,
+    private _activatedRoute: ActivatedRoute
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.betId = this._activatedRoute.snapshot.queryParams['betId'];
+    this.baseUrl = this._activatedRoute.snapshot.queryParams['baseUrl'];
+    this._renderer.setStyle(
+      this.gameLoadingBar?.nativeElement,
+      'display',
+      'block'
+    );
+    const script = this.loadGameScript();
+    this.gameContainer?.nativeElement?.appendChild(script);
+  }
+
+  private loadGameScript(): HTMLScriptElement {
+    const buildUrl = 'assets/power-up-game/Build';
+    const config = {
+      dataUrl: buildUrl + '/palmBet.data',
+      frameworkUrl: buildUrl + '/palmBet.framework.js',
+      codeUrl: buildUrl + '/palmBet.wasm',
+      streamingAssetsUrl: 'StreamingAssets',
+      companyName: 'FanStudio',
+      productName: 'UnityPalmerbet',
+      productVersion: '0.1.0',
+    };
+    const script = document.createElement('script');
+    script.async = false;
+    script.type = 'text/javascript';
+    script.src = 'assets/power-up-game/Build/palmBet.loader.js';
+    script.onload = () => {
+      createUnityInstance(
+        this.gameCanvas?.nativeElement,
+        config,
+        (progress: any) => {
+          const width = 100 * progress + '%';
+          this._renderer.setStyle(
+            this.gameProgressBarFull?.nativeElement,
+            'width',
+            width
+          );
+        }
+      )
+        .then((unityInstance: any) => {
+          this._renderer.setStyle(
+            this.gameLoadingBar?.nativeElement,
+            'display',
+            'none'
+          );
+          this.unityInstance = unityInstance;
+          window.handleUnityMessage = this.handleUnityMessage.bind(this);
+        })
+        .catch((message: any) => {
+          console.error(message);
+        });
+    };
+    return script;
+  }
+
+  private handleUnityMessage(message: string): void {
+    if (message === 'gameInfo') {
+      // this base url is used by game to make start game and end game api calls
+      const baseURL = this.baseUrl + '/';
+      const betId = this.betId;
+      const authorizationToken = `bearer`;
+
+      const obj = {
+        baseURL,
+        gameType: 'PowerUp',
+        betId,
+        authorizationToken,
+      };
+
+      const message = JSON.stringify(obj);
+      this.unityInstance?.SendMessage(
+        'ExternalMessaging',
+        'ReceiveMessage',
+        message
+      );
+    } else {
+      this._renderer.setStyle(
+        this.gameCanvas?.nativeElement?.parentElement?.parentElement,
+        'visibility',
+        'hidden'
+      );
+      this.unityInstance?.Quit();
+      this.unityInstance = null;
+    }
   }
 }
